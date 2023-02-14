400a605eb51de0f669e4529b59fc9e5ed3b96967
carlyyburton <carlyyburton@gmail.com>
2023-02-13 20:14:00 -0600

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'net/http'
+require 'json'
+
+api = 'https://www.cheapshark.com/api/1.0/deals'
+
+uri = URI(api)
+response = Net::HTTP.get(uri)
+
+games = JSON.parse(response)
+
+puts games
