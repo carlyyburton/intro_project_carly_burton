3028169ae0c942448beaa6e7e19f88e78198b230
carlyyburton <carlyyburton@gmail.com>
2023-02-28 15:07:27 -0600

    Updated game_sales view to only display data if it is there. Certain games do not have a steam rating or release date listed.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
-  validates :title, :sale_price, :normal_price, :release_date, presence: true
+  validates :title, :sale_price, :normal_price, presence: true
 
-  has_many :fake_games
+  #has_many :fake_games
 
   paginates_per 20
 end
